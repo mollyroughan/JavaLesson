243b82e8b2049fad8d86dd5b0674c3624bb57795
mollyroughan <mroughan@bu.edu>
2026-03-04 17:01:19 -0500

    Starting File

---
+++ JavaLesson.java
@@ -0,0 +1,13 @@
+Public class JavaLesson {
+
+  public static void main (string [] args) {
+    int 
+    int[] cities = ["Boston", "Phoenix", "New York City", "Dallas","Portland"]
+    int[] states = ["MA","AZ","NY","TX","OR"]
+    int[][] places = {cities, states}
+
+
+    
+  }
+  
+}
